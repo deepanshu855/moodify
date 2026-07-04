fee36fb6d4837f95719910e275299748f524c625
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-07-04 16:40:09 +0530

    Face expression detection setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './features/expression/components/FaceExpression'
+
+const App = () => {
+  return (
+    <main>
+      <FaceExpression />
+    </main>
+  )
+}
+
+export default App
