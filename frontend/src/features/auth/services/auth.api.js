3ccfb670f6039134c07a90a4a820a5cfb4524733
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-07-08 22:07:22 +0530

    API-update

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/auth/",
+  baseURL: "https://moodify-etat.onrender.com/api/auth/",
   withCredentials: true,
 });
 
